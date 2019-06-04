5e65fb95238e816f6543c0afd1e82a6b2eac564c
edustef <eduardstefan290@gmail.com>
2019-06-04 13:54:21 +0200

    Added login,logout functionality

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b29a5e61816e3c104500566e4909327d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db =  SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, url_for, flash, redirect
 from application.forms import RegisterForm, LoginForm
 from application.models import User 
-from application import app
+from application import app, bcrypt, db
+from flask_login import login_user, current_user, logout_user
 
 @app.route("/")
 @app.route("/home")
@@ -10,9 +11,13 @@ def home():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
 	form = LoginForm()
 	if form.validate_on_submit():
-		if form.username.data == 'Eduard' and form.password.data == 'admin':
+		user = User.query.filter_by(username='testadmin').first()
+		if user and bcrypt.check_password_hash(user.password, form.password.data):
+			login_user(user, remember=form.remember_me.data)
 			flash('Succesfuly logged in', 'success')
 			return redirect(url_for('home'))
 		else:
@@ -21,7 +26,22 @@ def login():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
 	form = RegisterForm()
 	if form.validate_on_submit():
-		flash(f'Account succesfuly created', 'success')
+		hash_pass = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+		user = User(username=form.username.data, email=form.email.data, password=hash_pass)
+		db.session.add(user)
+		db.session.commit()
+		flash(f'Your account has been succesfuly created. You can now log in.', 'success')
+		return redirect(url_for('login'))
 	return render_template("register.html", title='Register', form=form)
+
+@app.route("/logout")
+def logout():
+	logout_user()
+	return redirect(url_for('home'))
+
+@app.route("/account")
+def account():
