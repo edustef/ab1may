37ebb8b187d3fd61aa3705cfcc4c7aa66290d765
edustef <eduardstefan290@gmail.com>
2019-06-04 09:13:43 +0200

    created a user table

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegisterForm, LoginForm
@@ -9,6 +10,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db =  SQLAlchemy(app)
 
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), unique=True, nullable=False)
+	email = db.Column(db.String(120), unique=True, nullable=False)
+	password = db.Column(db.String(60), nullable=False)
+
+	def __repr__(self):
+		return f"User('{self.username}', '{self.email}')"
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -18,7 +28,7 @@ def home():
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
-		if form.email.data == 'admin@gmail.com' and form.password.data == 'admin':
+		if form.username.data == 'Eduard' and form.password.data == 'admin':
 			flash('Succesfuly logged in', 'success')
 			return redirect(url_for('home'))
 		else:
