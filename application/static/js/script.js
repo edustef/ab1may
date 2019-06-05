85c5c8bf29fca3c6f5e2c3443bd661b2b12cd1ea
edustef <eduardstefan290@gmail.com>
2019-06-05 11:14:44 +0200

    fixed links to external pages not working

--- application/static/js/script.js
+++ application/static/js/script.js
@@ -14,9 +14,10 @@ function showLess() {
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
-
-  // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== "") {
+    // Make sure this.hash has a value before overriding default behavior
+    let this_address = this.href.replace(this.hash,'');
+    let link_address = window.location.href.replace(window.location.hash,'');
+    if (this.hash !== "" && this_address == link_address) {
       // Prevent default anchor click behavior
       event.preventDefault();
 
@@ -26,12 +27,11 @@ $(document).ready(function(){
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
-      scrollTop: $(hash).offset().top
+        scrollTop: $(hash).offset().top
       }, 800, function(){
-
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
       });
-  } // End if
+    } // End if
   });
 });
