6fa7c63de2ea257c0b31a694e22990d95ad028b3
edustef <eduardstefan290@gmail.com>
2019-06-10 11:27:01 +0200

    Added post class

--- application/models.py
+++ application/models.py
@@ -1,6 +1,6 @@
 from application import db, login_manager
 from flask_login import UserMixin
-
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -11,6 +11,16 @@ class User(db.Model, UserMixin):
 	username = db.Column(db.String(20), unique=True, nullable=False)
 	email = db.Column(db.String(120), unique=True, nullable=False)
 	password = db.Column(db.String(60), nullable=False)
+	posts = db.relationship('Post', backref='poster', lazy=True)
 
 	def __repr__(self):
 		return f"User('{self.username}', '{self.email}')"
+
+class Post(db.Model, UserMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	image_file = db.Column(db.String(35), nullable=False)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+	def __repr__(self):
+		return f"Post('{self.id}', '{self.image_file}', '{self.date_posted}')"
