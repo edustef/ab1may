68984d71ffc72018a0b12dbf30b4221095c854a7
edustef <eduardstefan290@gmail.com>
2019-06-15 22:09:02 +0200

    Created the update validation in routes.py

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect, request
-from application.forms import RegisterForm, LoginForm
+from application.forms import RegisterForm, LoginForm, UpdateAccountForm
 from application.models import User 
 from application import app, bcrypt, db
 from flask_login import login_user, current_user, logout_user, login_required
@@ -44,10 +44,21 @@ def logout():
 	logout_user()
 	return redirect(url_for('index'))
 
-@app.route("/account")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-	return render_template("account.html", title='Profile')
+	form = UpdateAccountForm()
+	if form.validate_on_submit():
+		current_user.username = form.username.data
+		current_user.email = form.email.data
+		db.session.commit()
+		flash('Your account has been succesfuly updated.', 'success')
+		return redirect(url_for('account'))
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.email.data = current_user.email
+	avatar_img = url_for('static', filename='profile_pics/' + current_user.avatar_img)
+	return render_template("account.html", title='Profile', avatar_img=avatar_img, form=form)
 
 @app.route("/data")
 @login_required
