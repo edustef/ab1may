68c3a4983d02b753752b150483eec5f34dc4b173
edustef <eduardstefan290@gmail.com>
2019-06-15 22:08:08 +0200

    added avatar in db

--- application/models.py
+++ application/models.py
@@ -10,17 +10,17 @@ class User(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(20), unique=True, nullable=False)
 	email = db.Column(db.String(120), unique=True, nullable=False)
+	avatar_img = db.Column(db.String(35), nullable=False, default='default.png')
 	password = db.Column(db.String(60), nullable=False)
 	posts = db.relationship('Post', backref='poster', lazy=True)
 
 	def __repr__(self):
-		return f"User('{self.username}', '{self.email}')"
+		return f"User('{self.username}', '{self.email}', '{self.avatar_img}')"
 
 class Post(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
-	image_file = db.Column(db.String(35), nullable=False)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 	def __repr__(self):
-		return f"Post('{self.id}', '{self.image_file}', '{self.date_posted}')"
+		return f"Post('{self.id}', '{self.date_posted}')"
